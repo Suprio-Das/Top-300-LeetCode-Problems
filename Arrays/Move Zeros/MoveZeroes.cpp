a5bd9a3307fab02a99c16ecd13ec8a30909ece3d
Suprio-Das <priyamd507@gmail.com>
2025-04-10 23:54:05 +0600

    Moving Zeroes solved and added

---
+++ Arrays/Move Zeros/MoveZeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> arr;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                arr.push_back(nums[i]);
+            }
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0){
+                arr.push_back(nums[i]);
+            }
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = arr[i];
+        }
+    }
+};
