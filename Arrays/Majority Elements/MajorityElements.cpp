6cb3e18f9f2be790d633e86a3d6e8987f073b491
Suprio-Das <priyamd507@gmail.com>
2025-04-11 13:29:39 +0600

    Count variable added

--- Arrays/Majority Elements/MajorityElements.cpp
+++ Arrays/Majority Elements/MajorityElements.cpp
@@ -4,6 +4,7 @@ public:
     int majorityElement(vector<int> &nums)
     {
         int n = nums.size();
+        int count = 0;
         vector<int> checked;
 
         for (int i = 0; i < n - 1; i++)
