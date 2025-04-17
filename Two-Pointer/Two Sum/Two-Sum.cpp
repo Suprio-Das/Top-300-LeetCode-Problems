80d368673e9011521383a36e7fcd0feb1eb43f2e
Suprio-Das <priyamd507@gmail.com>
2025-04-17 22:10:19 +0600

    Two pointers - Left and Right added

--- Two-Pointer/Two Sum/Two-Sum.cpp
+++ Two-Pointer/Two Sum/Two-Sum.cpp
@@ -3,6 +3,8 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &numbers, int target)
     {
+        int pointer_left = 0;
+        int pointer_right = numbers.size() - 1;
         return {};
     }
 };
