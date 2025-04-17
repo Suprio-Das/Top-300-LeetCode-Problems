899ea402166b46576e1cb5a5a61b3e7e8a0081f8
Suprio-Das <priyamd507@gmail.com>
2025-04-17 22:09:18 +0600

    Two sum problem created

--- Arrays/Move Zeros/MoveZeroes.cpp
+++ Arrays/Move Zeros/MoveZeroes.cpp
@@ -1,19 +1,7 @@
-class Solution {
+class Solution
+{
 public:
-    void moveZeroes(vector<int>& nums) {
-        vector<int> arr;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0){
-                arr.push_back(nums[i]);
-            }
-        }
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == 0){
-                arr.push_back(nums[i]);
-            }
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] = arr[i];
-        }
-    }
+    void moveZeroes(vector<int> &nums)
+    {
+    }
 };
---
+++ Two-Pointer/Two Sum/Two-Sum.cpp
@@ -0,0 +1,8 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        return {};
+    }
+};
