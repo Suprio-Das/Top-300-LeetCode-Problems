8bb423c0917ad43b2b68cdbdb33179a02c5ffe79
Suprio-Das <priyamd507@gmail.com>
2025-04-20 23:14:44 +0600

    Problem solved

--- Arrays/Product of Array Except Self/ProductOfArrayExceptSelf.cpp
+++ Arrays/Product of Array Except Self/ProductOfArrayExceptSelf.cpp
@@ -5,5 +5,21 @@ public:
     {
         int n = nums.size();
         vector<int> answer(n, 1);
+
+        int prefix = 1;
+        for (int i = 0; i < n; i++)
+        {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return answer;
     }
 };
