71cd464991c3f2047e6c9dd31931cd6c5071e75f
Suprio-Das <priyamd507@gmail.com>
2025-04-22 22:50:58 +0600

    Condition added

--- Arrays/Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II.cpp
+++ Arrays/Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II.cpp
@@ -6,6 +6,9 @@ public:
         int maxProfit = 0;
         for (int i = 1; i < prices.size(); i++)
         {
+            if (prices[i] > prices[i - 1])
+            {
+            }
         }
     }
 };
